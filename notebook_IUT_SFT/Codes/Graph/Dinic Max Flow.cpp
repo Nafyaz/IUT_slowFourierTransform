4bdc376d9b1619bcff2c1d3e16afbf4f86208582
CSE <nafis.faiyaz.5@gmail.com>
2023-01-26 22:35:04 +0600

    added FLB

--- notebook_IUT_SFT/Codes/Graph/Dinic Max Flow.cpp
+++ notebook_IUT_SFT/Codes/Graph/Dinic Max Flow.cpp
@@ -1,3 +1,11 @@
+///flow with demand(lower bound) only for DAG
+//create new src and sink
+//add_edge(new src, u, sum(in_demand[u]))
+//add_edge(u, new sink, sum(out_demand[u]))
+//add_edge(old sink, old src, inf)
+// if (sum of lower bound == flow) then demand satisfied
+//flow in every edge i = demand[i] + e.flow
+
 using Ti = long long;
 const Ti INF = 1LL << 60;
 struct edge {
